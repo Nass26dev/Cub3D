9b195e6a331acdaf07cd2467d6e5d0ffe21793bd
Ru2ner <mtheo70400@gmail.com>
2025-08-18 17:43:10 +0200

    Libft_updtate

--- Libft/libft.h
+++ Libft/libft.h
@@ -49,7 +49,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 int		ft_strncmp(const char *s1, const char *s2, size_t nb);
-int		ft_strnstr(const char *find, const char *tofind, size_t nb);
+char	*ft_strnstr(const char *find, const char *tofind, size_t nb);
 char	*ft_strrchr(const char *find, int tofind);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
