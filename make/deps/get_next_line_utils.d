257d0ad028441d85972e649e464c0d3fbb758d6a
Nassim Yousfi <nyousfi@z3r4p1.42lyon.fr>
2025-08-26 09:45:33 +0200

    rm .o

--- make/deps/get_next_line_utils.d
+++
@@ -1,3 +0,0 @@
-make/objs/get_next_line_utils.o: src/get_next_line_utils.c \
- include/get_next_line.h
-include/get_next_line.h:
--- make/deps/player.d
+++
@@ -1,5 +0,0 @@
-make/objs/player.o: src/player.c include/cub3D.h \
- include/../minilibx/mlx.h include/../Libft/libft.h
-include/cub3D.h:
-include/../minilibx/mlx.h:
-include/../Libft/libft.h:
