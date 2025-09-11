fdee55b75b2f9ae21efb89c9a40d211bb808cd3c
Ru2ner <mtheo70400@gmail.com>
2025-09-11 12:50:15 +0200

    save

--- make/deps/is_valid.d
+++
@@ -1,8 +0,0 @@
-make/objs/is_valid.o: src/is_valid.c include/cub3D.h \
-  include/../minilibx/mlx.h include/../Libft/libft.h
-
-include/cub3D.h:
-
-include/../minilibx/mlx.h:
-
-include/../Libft/libft.h:
